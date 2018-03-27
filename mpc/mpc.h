1c1539eb8dfbdf80d01b8ee54cc71855cf05b2fa
Juan Carlos Ortiz <ortizjuan2@gmail.com>
2018-03-26 22:46:51 -0500

    feat:	Implementing FG_eval class
    Implementing the FG_eval class to solve the NLP.

--- mpc/mpc.h
+++ mpc/mpc.h
@@ -2,3 +2,31 @@
  *  
  *  \brief Model Predictive Controller header file
  */
+
+#ifndef MPC_H_
+#define MPC_H_
+
+#include <cppad/cppad.hpp>
+
+//using namespace CppAD::AD;
+
+class FG_eval {
+public:
+    typedef CPPAD_TESTVECTOR(CppAD::AD<double>) ADvector;
+    
+    void operator()(ADvector & fg, const ADvector & x){
+//        assert(fg.size() == 3);
+        AD<double> x1 = x[0];
+        
+        //f(x) or cost function
+        fg[0] = x1 ....;
+        //constraints
+        fg[1] = constraint....
+        // second constraint
+                fg[2] = constraint....
+                return;
+    }
+};
+
+
+#endif
